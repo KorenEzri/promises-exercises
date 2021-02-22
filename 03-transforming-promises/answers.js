0289844c2ee57fda70b935bd2330c65a70833a9f
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-02-22 17:00:24 +0200

    finishhh

--- 03-transforming-promises/answers.js
+++ 03-transforming-promises/answers.js
@@ -1,48 +1,73 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {*} promise 
- * @param {*} transformer 
+ *
+ * @param {*} promise
+ * @param {*} transformer
  * @returns {Promise}
  */
-function mapPromise(promise, transformer){
+function mapPromise(promise, transformer) {
   return new Promise((resolve, reject) => {
-    /* IMPLEMENT ME!! */
+    promise
+      .catch((err) => reject(err))
+      .then((result) => {
+        try {
+          return resolve(transformer(result));
+        } catch (error) {
+          return reject(error);
+        }
+      });
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromise(numberPromise){
-  return numberPromise
-    .then(/* IMPLEMENT ME! */);
+function squarePromise(numberPromise) {
+  return numberPromise.then(
+    (result) =>
+      new Promise((resolve, reject) => {
+        if (Number(result)) {
+          resolve(result * result);
+        } else !Number(result);
+        reject(`Cannot convert '${result}' to a number!`);
+      })
+  );
 }
 
 /**
  * EXERCISE 3
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromiseOrZero(promise){
-  return squarePromise(promise)
-    .catch(/* IMPLEMENT ME! */);
+function squarePromiseOrZero(promise) {
+  return squarePromise(promise).catch(() => {
+    if (Number(promise)) {
+      return promise;
+    } else return 0;
+  });
 }
 
 /**
  * EXERCISE 4
- * 
- * @param {Promise} promise 
+ *
+ * @param {Promise} promise
  * @returns {Promise}
  */
-function switcheroo(promise){
-  return promise.then(/* IMPLEMENT ME */);
+function switcheroo(promise) {
+  return promise.then(
+    (onSuccess) => {
+      throw onSuccess;
+    },
+    (onFailure) => {
+      return onFailure;
+    }
+  );
 }
 
 /**
